60455c6099942c6b1050ed4a2e0a482337145871
Ignacio Talvi <ignaciotalvi@Ignacios-MacBook-Pro.local>
2025-03-25 09:19:21 +0100

    completed excercises 1 and 2,3,4,5

--- index.js
+++ index.js
@@ -19,3 +19,21 @@ function getRandomDog() {
 console.log(getRandomDog());
 
 // EJERCICIO 3
+function getAllImagesByBreed() {
+  return fetch("https://dog.ceo/api/breed/komondor/images")
+    .then((res) => res.json())
+    .then((data) => data.message);
+}
+
+console.log(getAllImagesByBreed());
+
+// EJERCICIO 4
+function getAllImagesByBreed2(raza) {
+  return fetch(`https://dog.ceo/api/breed/${raza}/images`)
+    .then((res) => res.json())
+    .then((data) => data.message);
+}
+
+console.log(getAllImagesByBreed2(komondor));
+
+// EJERCICIO 5
